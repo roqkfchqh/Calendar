989195734206a034622168247653790156984651
roqkfchqh <plmokn48@daum.net>
2024-12-11 20:15:31 +0900

    user 삭제될 때 관련 calendar 함께 삭제되게 수정

--- src/main/java/com/calendar/controller/user/service/UserService.java
+++ src/main/java/com/calendar/controller/user/service/UserService.java
@@ -1,18 +1,22 @@
 package com.calendar.controller.user.service;
 
 import com.calendar.common.exception.BadInputException;
+import com.calendar.controller.calendar.repository.CalendarRepository;
 import com.calendar.controller.user.dto.*;
 import com.calendar.controller.user.model.User;
 import com.calendar.controller.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserService {
 
     private final UserRepository userRepository;
+    private final CalendarRepository calendarRepository;
     private final UserValidationService userValidationService;
 
     //read
@@ -38,6 +42,7 @@ public class UserService {
     public void deleteUser(HttpServletRequest req, CurrentPasswordRequestDto currentPasswordRequestDto){
         User user = userValidationService.validateUser(req);
         userValidationService.validatePassword(user.getPassword(), currentPasswordRequestDto.getCurrentPassword());
+        calendarRepository.deleteByUserId(user.getId());
         userRepository.deleteById(user.getId());
     }
 
