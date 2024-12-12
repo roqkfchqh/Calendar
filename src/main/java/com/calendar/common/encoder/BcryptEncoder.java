cc194bdc31f1f59041957221a6fbffbff15681de
roqkfchqh <plmokn48@daum.net>
2024-12-12 16:47:59 +0900

    오타 수정

--- src/main/java/com/calendar/common/encoder/BcryptEncoder.java
+++ src/main/java/com/calendar/common/encoder/BcryptEncoder.java
@@ -1,18 +1,15 @@
 package com.calendar.common.encoder;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BcryptEncoder {
 
-    @Bean
     public String encode(String rawPassword) {
         return BCrypt.withDefaults().hashToString(BCrypt.MIN_COST, rawPassword.toCharArray());
     }
 
-    @Bean
     public boolean matches(String rawPassword, String encodedPassword) {
         BCrypt.Result result = BCrypt.verifyer().verify(rawPassword.toCharArray(), encodedPassword);
         return result.verified;
